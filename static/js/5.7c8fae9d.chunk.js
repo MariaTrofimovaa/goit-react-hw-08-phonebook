0252dd76d11108c7b7a41f0feaeb7b39f0acd0a1
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-07-06 18:46:14 +0300

    Updates

---
+++ static/js/5.7c8fae9d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpt=this.webpackJsonpt||[]).push([[5],{111:function(t,e,n){"use strict";n.r(e);n(0);var c=n(86),i=n.n(c),o=n(1);e.default=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{className:i.a.title,children:"Welcome to Phonebook"})})}},86:function(t,e,n){t.exports={title:"HomePage_title__1MZYr"}}}]);
+//# sourceMappingURL=5.7c8fae9d.chunk.js.map
